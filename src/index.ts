e7bc50349a2b355f99207ac7c4c1fcf49ca178bb
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2018-01-09 15:58:33 +0100

    lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import "./app.scss";
 import { version } from "punycode";
 
-// Show versions of backend and frontend. 
+// Show versions of backend and frontend.
 // You can safely remove this
 Promise.all(['/api/version', '/VERSION'].map(path =>
   fetch(path)
     .then(resp => resp.text())
-    .catch(error => error.message)
+    .catch(error => error.message),
 )).then((versions: string[]) => {
-  document.body.innerHTML = `Backend: ${versions[0]}, Frontend: ${versions[1]}`
+  document.body.innerHTML = `Backend: ${versions[0]}, Frontend: ${versions[1]}`;
 });
