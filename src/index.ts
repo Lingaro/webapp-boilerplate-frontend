4a74f2952d6c8004a524606c0bd8d63b0cb89db6
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2018-01-09 14:43:22 +0100

    tslint

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import "./app.scss";
 fetch("/api/version")
   .then(resp => resp.text())
   .catch(error => JSON.stringify(error))
-  .then(version => document.body.innerHTML = 'Backend ver: '+version);
+  .then(version => document.body.innerHTML = 'Backend ver: ' + version);
